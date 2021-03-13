5a06b0ccdf33d5a299a498eecae3e9beda7ce89d
sam <sam@freighttrust.com>
2021-03-12 22:22:27 -0800

    feat(sw): web3
    Signed-off-by: sam <sam@freighttrust.com>

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,31 +1,31 @@
-import resolve from "@rollup/plugin-node-resolve";
-import html from "@open-wc/rollup-plugin-html";
-import copy from "rollup-plugin-copy";
-import replace from "@rollup/plugin-replace";
-import typescript from "@rollup/plugin-typescript";
+import resolve from '@rollup/plugin-node-resolve';
+import html from '@open-wc/rollup-plugin-html';
+import copy from 'rollup-plugin-copy';
+import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: "index.html",
+  input: 'index.html',
   output: {
-    dir: "build",
-    format: "es",
+    dir: 'build',
+    format: 'es',
   },
   plugins: [
     resolve(),
     html(),
     typescript({
-      tsconfig: "tsconfig.dev.json",
+      tsconfig: 'tsconfig.dev.json',
     }),
     replace({
-      "process.env.NODE_ENV": JSON.stringify(
-        process.env.NODE_ENV || "production"
-      )
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV || 'production'
+      ),
     }),
     copy({
       targets: [
-        { src: "assets/**/*", dest: "build/assets/" },
-        { src: "styles/global.css", dest: "build/styles/" },
-        { src: "manifest.json", dest: "build/" },
+        { src: 'assets/**/*', dest: 'build/assets/' },
+        { src: 'styles/global.css', dest: 'build/styles/' },
+        { src: 'manifest.json', dest: 'build/' },
       ],
     }),
   ],
--- web3background-queue.ts
+++ web3background-queue.ts
@@ -1,7 +1,7 @@
-import {BackgroundSyncPlugin} from 'workbox-background-sync';
-import {registerRoute} from 'workbox-routing';
-import {NetworkOnly} from 'workbox-strategies';
-import {Queue} from 'workbox-background-sync';
+import { BackgroundSyncPlugin } from 'workbox-background-sync';
+import { registerRoute } from 'workbox-routing';
+import { NetworkOnly } from 'workbox-strategies';
+import { Queue } from 'workbox-background-sync';
 
 const queue = new Queue('myQueueName');
 
@@ -9,21 +9,20 @@ self.addEventListener('fetch', (event) => {
   // Clone the request to ensure it's safe to read when
   // adding to the Queue.
   const promiseChain = fetch(event.request.clone()).catch((err) => {
-    return queue.pushRequest({request: event.request});
+    return queue.pushRequest({ request: event.request });
   });
 
   event.waitUntil(promiseChain);
 });
 
-
 const bgSyncPlugin = new BackgroundSyncPlugin('myQueueName', {
-  maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
 });
 
 registerRoute(
   /\/api\/.*\/*.json/,
   new NetworkOnly({
-    plugins: [bgSyncPlugin]
+    plugins: [bgSyncPlugin],
   }),
   'POST'
 );
