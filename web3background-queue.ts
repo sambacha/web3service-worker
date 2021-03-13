1bb0bba3a64c44b6a1e943cb2e03572e184d74f4
sam <sam@freighttrust.com>
2021-03-12 22:22:09 -0800

    v0.1.0
    Signed-off-by: sam <sam@freighttrust.com>

---
+++ web3background-queue.ts
@@ -0,0 +1,29 @@
+import {BackgroundSyncPlugin} from 'workbox-background-sync';
+import {registerRoute} from 'workbox-routing';
+import {NetworkOnly} from 'workbox-strategies';
+import {Queue} from 'workbox-background-sync';
+
+const queue = new Queue('myQueueName');
+
+self.addEventListener('fetch', (event) => {
+  // Clone the request to ensure it's safe to read when
+  // adding to the Queue.
+  const promiseChain = fetch(event.request.clone()).catch((err) => {
+    return queue.pushRequest({request: event.request});
+  });
+
+  event.waitUntil(promiseChain);
+});
+
+
+const bgSyncPlugin = new BackgroundSyncPlugin('myQueueName', {
+  maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+  /\/api\/.*\/*.json/,
+  new NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  'POST'
+);
